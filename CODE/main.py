076bba641b7c872fb99013b11ba3b60d50468646
Alon Malka <alon1malka@gmail.com>
2023-07-26 09:55:30 +0300

    Name Changes

---
+++ CODE/main.py
@@ -0,0 +1,166 @@
+import os
+
+import neat.config
+
+import settings
+import numpy
+import pygame
+import numpy as np
+import math
+import pandas as pd
+from utils import *
+from AbstractCar import *
+import create_road
+
+
+def draw(_win, _player_cars):
+    """
+    Draws the boarder, cars, sensors and bonus line
+    :param _win: the window (pygame object)
+    :param _player_cars: the cars to be drawn
+    :return:  nothing
+    """
+    _win.fill("black")
+    i = 0
+
+    for bonus_line in settings.BONUS_LINES:
+
+        if i % 6 == 0:
+            rgb = (255,0,0)
+        elif i % 6 == 1:
+            rgb = (255,160,0)
+        elif i % 6 == 2:
+            rgb = (255,255,0)
+        elif i % 6 == 3:
+            rgb = (0,255,0)
+        elif i % 6 == 4:
+            rgb = (0,0,255)
+        elif i % 6 == 5:
+            rgb = (100,0,200)
+        pygame.draw.line(_win, rgb, *bonus_line)
+        i+=1
+    i = 1
+
+    for track_line in settings.TRACK_LINES:
+        i += 1
+        pygame.draw.line(_win, (255,255,255), *track_line)
+
+    for __car in _player_cars:
+        __car.draw(_win)
+    pygame.display.update()
+
+
+def main(genomes, config):
+    """
+    The main function NEAT is running
+    :param genomes: The objects of the cars in NEAT form
+    :param config: the configuration file named "config-feedforward.txt"
+    :return: nothing
+    """
+
+    nets = []
+    ge = []
+    cars = []
+    # _car_array.append(PlayerCar(max_vel, rotation_vel, twi, tbi, tw1, tb1))
+
+    for _, g in genomes:
+        # print("bazinga")
+        net = neat.nn.FeedForwardNetwork.create(g, config)
+        nets.append(net)
+        cars.append(PlayerCar(settings.MAX_VEL, settings.ROTATION_VEL))
+        g.fitness = 0
+        ge.append(g)
+
+    run = True
+
+    clock = pygame.time.Clock()
+
+    runs = 0
+
+    win = pygame.display.set_mode(settings.WIN_SHAPE)
+    while run:
+        # print("buya", len(cars))
+        clock.tick(settings.FPS)
+        pygame.display.update()
+        draw(win, cars)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                pygame.quit()
+
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                pos = pygame.mouse.get_pos()
+                pygame.draw.circle(win, (0, 255, 0), pos, 2)
+                print(pos)
+        if len(cars) != 0:
+            runs += 1
+            for x, car in enumerate(cars):
+
+                ge[x].fitness -= 10
+
+                car.update_input_layer()
+                output = nets[x].activate(car.input_layer)
+                car.take_action(output)
+                car.move()
+                for point in car.points_sensor:
+                    if point is not None:
+                        color, center, radius = point
+                        center_x, center_y = center
+                        pygame.draw.circle(win, color, (int(center_x), int(center_y)), radius)
+                pygame.display.update()
+                if car.collide() or runs >= 100 + settings.FRAMES_PER_BONUS_LINE * car.index_of_bonus_line +\
+                        settings.FRAMES_PER_BONUS_LINE * car.rounds_completed * len(settings.BONUS_LINES):
+
+                    ge[x].fitness -= 5000
+
+                    cars.pop(x)
+                    nets.pop(x)
+                    ge.pop(x)
+
+                else:
+                    rect = car.img.get_rect(topleft=(car.x, car.y))
+                    if rect.clipline(car.next_bonus_line):
+                        if car.index_of_bonus_line >= len(settings.BONUS_LINES) - 2:
+                            car.rounds_completed += 1
+                            car.index_of_bonus_line = 0
+                        # print("BONUS!")
+                        ge[x].fitness += 1000
+                        car.index_of_bonus_line += 1
+                        car.next_bonus_line = settings.BONUS_LINES[car.index_of_bonus_line]
+                        # print("this is my next bonus line,", car.next_bonus_line)
+
+        else:
+            break
+
+
+def run(_config_path):
+    """
+    For NEAT. Runs the NEAT algorithm and logs the progres.
+    :param _config_path: The path of the config file
+    :return: Nothing
+    """
+    config = neat.config.Config(neat.DefaultGenome, neat.DefaultReproduction, neat.DefaultSpeciesSet
+                                , neat.DefaultStagnation, _config_path)
+    p = neat.Population(config)
+
+    p.add_reporter(neat.StdOutReporter(True))
+    stats = neat.StatisticsReporter()
+    p.add_reporter(stats)
+
+    winner = p.run(main, 1000)
+    print(winner, "winner")
+
+
+if __name__ == "__main__":
+    track_lines, bonus_lines, starting_position, win_size = create_road.main()
+    settings.TRACK_LINES = track_lines
+    settings.BONUS_LINES = bonus_lines
+    settings.STARTING_POSITION = starting_position
+    print("win size", win_size)
+    settings.WIN_SHAPE = win_size
+    print("starting position", starting_position)
+    print("bonus lines", settings.BONUS_LINES)
+
+    local_dir = os.path.dirname(__file__)
+    config_path = os.path.join(local_dir, "config-feedforward.txt")
+    run(config_path)
